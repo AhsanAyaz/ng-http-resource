7ad2c78010e614c4afc68ea493ee28ec575c8de6
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2025-03-03 05:04:19 +0100

    chore(addTodo): implement adding todos (not how it should be done)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,30 @@
-import { Component, effect, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UsersService } from './users.service';
 import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 import { TodosService } from './todos.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoadingSpinnerComponent],
+  imports: [RouterOutlet, LoadingSpinnerComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   usersService = inject(UsersService);
   todosService = inject(TodosService);
+  newTodoInput = signal('');
 
-  constructor() {
-    effect(() => {
-      const users = this.usersService.usersResource.value();
-      if (users.length && !this.todosService.user()) {
-        this.todosService.user.set(users[0]);
-      }
+  addNewTodo() {
+    const title = this.newTodoInput();
+    if (!title) {
+      return;
+    }
+    this.todosService.newTodoInput.set({
+      completed: false,
+      title,
     });
+    this.newTodoInput.set('');
   }
 }
--- src/app/todos.service.ts
+++ src/app/todos.service.ts
@@ -14,15 +14,48 @@ export type Todo = {
 })
 export class TodosService {
   user = signal<User | null>(null);
-  todosResource = httpResource<Todo[]>(() => {
-    const user = this.user();
-    if (!user) {
-      return undefined;
+  newTodoInput = signal<Partial<Todo> | null>(null);
+  todosResource = httpResource<Todo[]>(
+    () => {
+      const user = this.user();
+      if (!user) {
+        return undefined;
+      }
+      return `https://jsonplaceholder.typicode.com/users/${
+        this.user()?.id
+      }/todos`;
+    },
+    {
+      defaultValue: [],
     }
-    return `https://jsonplaceholder.typicode.com/users/${
-      this.user()?.id
-    }/todos`;
-  });
+  );
+
+  newTodoResource = httpResource<Todo>(
+    () => {
+      const user = this.user();
+      if (!user || !this.newTodoInput()) {
+        return undefined;
+      }
+      return {
+        url: `https://jsonplaceholder.typicode.com/users/${
+          this.user()?.id
+        }/todos`,
+        method: 'POST',
+        body: this.newTodoInput(),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+    },
+    {
+      parse: (val) => {
+        const newTodo = val as Todo;
+        this.todosResource.update((list) => [newTodo, ...list]);
+        this.newTodoInput.set(null);
+        return newTodo;
+      },
+    }
+  );
 
   todosResourceError = computed(() => {
     const error = this.todosResource.error() as Error;
